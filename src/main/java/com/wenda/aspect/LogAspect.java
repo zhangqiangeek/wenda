04c0a2b06e95a2793fd4c28522bbba3e07b0e5c2
zhangqian <1204862040@qq.com>
2017-09-03 21:58:06 +0800

    Spring

---
+++ src/main/java/com/wenda/aspect/LogAspect.java
@@ -0,0 +1,41 @@
+package com.wenda.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by zq on 2017/9/3.
+ */
+@Aspect
+@Component
+public class LogAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LogAspect.class);
+
+    /**
+     * controller处理之前
+     *
+     * @param joinPoint
+     */
+    @Before("execution(* com.wenda.controller.*.*(..))")
+    public void beforeMethod(JoinPoint joinPoint) {
+        StringBuilder sb = new StringBuilder();
+        for (Object arg : joinPoint.getArgs()) {
+            sb.append("arg: " + arg.toString() + "|");
+        }
+        logger.info("before method:" + sb.toString());
+
+    }
+
+    /**
+     * controller处理之后
+     */
+    @After("execution(* com.wenda.controller.*.*(..))")
+    public void afterMethod() {
+        logger.info("after method");
+    }
+}
--- src/main/java/com/wenda/controller/IndexController.java
+++ src/main/java/com/wenda/controller/IndexController.java
@@ -1,34 +1,116 @@
 package com.wenda.controller;
 
+import com.wenda.model.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
- * Created by nowcoder on 2016/7/10.
+ * Created by zq on 2016/7/10.
  */
 @Controller
 public class IndexController {
-    
-    @RequestMapping(path = {"/", "/index"}, method = {RequestMethod.GET})
+
+    @RequestMapping(path = { "/", "/index" }, method = { RequestMethod.GET })
     @ResponseBody
-    public String index( ) {
-        return "hello spring boot";
+    public String index(HttpSession session) {
+        return "hello spring boot " + session.getAttribute("msg");
     }
 
-    @RequestMapping(path = {"/profile/{groupId}/{userId}"})
+    @RequestMapping(path = { "/profile/{groupId}/{userId}" })
     @ResponseBody
-    public String profile(@PathVariable("userId") int userId,
-                          @PathVariable("groupId") String groupId,
-                          @RequestParam(value = "type", defaultValue = "1") int type,
-                          @RequestParam(value = "key", required = false) String key) {
+    public String profile(@PathVariable("userId") int userId, @PathVariable("groupId") String groupId,
+            @RequestParam(value = "type", defaultValue = "1") int type, @RequestParam(value = "key", required = false) String key) {
         return String.format("Profile Page of %s / %d, t:%d k: %s", groupId, userId, type, key);
     }
 
-    @RequestMapping(path = {"/vm"}, method = {RequestMethod.GET})
-    public String template( ) {
+    /**
+     * 模板
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(path = { "/vm" }, method = { RequestMethod.GET })
+    public String home(Model model) {
+
+        User user = new User("zhang");
+        model.addAttribute("value", "value");
+        model.addAttribute("user", user);
+
         return "home";
     }
 
+    /**
+     * request 和response
+     *
+     * @param model
+     * @param response
+     * @param request
+     * @param session
+     * @param sessionId
+     * @return
+     */
+    @RequestMapping(path = { "/request" }, method = { RequestMethod.GET })
+    @ResponseBody
+    public String template(Model model, HttpServletResponse response, HttpServletRequest request, HttpSession session,
+            @CookieValue("JSESSIONID") String sessionId) {
+        StringBuilder sb = new StringBuilder();
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                sb.append("Cookie:" + cookie.getName() + "value:" + cookie.getValue() + "<br>");
+            }
+        }
 
+        sb.append(sessionId);
+        sb.append(request.getMethod() + "<br>");
+        sb.append(request.getCookies() + "<br>");
+        sb.append(request.getAuthType() + "<br>");
+        sb.append(request.getRequestURL() + "<br>");
+        sb.append(request.getSession() + "<br>");
+
+        response.addHeader("test", "hello its test");
+        response.addCookie(new Cookie("userName", "userName"));
+
+        return sb.toString();
+    }
+
+    /**
+     * 301跳转
+     *
+     * @param code
+     * @param session
+     * @return
+     */
+    @RequestMapping(path = { "/redirect/{code}" }, method = { RequestMethod.GET })
+    public RedirectView redirect(@PathVariable("code") int code, HttpSession session) {
+        session.setAttribute("msg", "jump from redirect!");
+        RedirectView redirectView = new RedirectView("/", true);
+        if (code == 301) {
+            redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        }
+        return redirectView;
+    }
+
+    @RequestMapping(path = "/admin", method = { RequestMethod.GET })
+    @ResponseBody
+    public String admin(@RequestParam("key") String key) {
+        if (key.equals("admin")) {
+            return "hello admin";
+        }
+        throw new IllegalArgumentException("参数不对");
+    }
+
+    @ExceptionHandler()
+    @ResponseBody
+    public String error(Exception e) {
+        return "error" + e.getMessage();
+    }
 
 }
